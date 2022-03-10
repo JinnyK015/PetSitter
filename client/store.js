d7f86b01718498f59ee093f103329ac9180fa70b
jinny <vedaforest@gmail.com>
2022-03-10 22:35:34 +1300

    init

--- client/store.js
+++ client/store.js
@@ -5,8 +5,6 @@ import thunk from 'redux-thunk'
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)))
 
-
 export default store
 export const getState = store.getState
 export const dispatch = store.dispatch
-
