f2e8dbe6b931ff7fb97ca15f78473f6be1b8d142
jinny <vedaforest@gmail.com>
2022-03-11 16:21:59 +1300

    init again

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: ['./client/index.js', './client/styles/index.scss'],
@@ -14,8 +13,7 @@ module.exports = {
       filename: 'styles.css',
       chunkFilename: '[id].css',
       ignoreOrder: false // Enable to remove warnings about conflicting order,
-    }),
-    new Dotenv()
+    })
   ],
   module: {
     rules: [
